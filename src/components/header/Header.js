15244f4e811f16ac6b5906907e4eacfa3c26756c
Thomas <taulo@live.dk>
2022-09-07 21:19:21 +0200

    working on small feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect } from "react"
 import "./Header.css"
+import { useLocation } from "react-router-dom";
 import {useNavigate, Link} from "react-router-dom"
 
-function Header() {
+function Header({ history }) {
     const navigate = useNavigate();
+    const location = useLocation()
 
-    const goToAbout = (e) => {
+    let currentElement = null
+
+    function goToAbout() {
         const aboutElement = document.getElementsByClassName("main-container")[0]
         if (aboutElement !== null && aboutElement !== undefined) {
             aboutElement.scrollIntoView({behavior:"smooth"})
@@ -17,16 +21,27 @@ function Header() {
         }
     }
 
+    // useEffect(() => {
+    //     if (currentElement !== null) currentElement.style.textDecoration = "underline"
+    // }, [location])
+
+    // remove underline when at the current route
+    function removeUnderline(e) {
+        // const elementClassName = e.target.className
+        // currentElement = document.querySelector(`.${elementClassName}`)
+        // currentElement.style.textDecoration = "none"
+    }
+
     return ( 
         <div className="header">
             <div className="header-home-text-container">
                 <h1 id="header-home-text" onClick={() => navigate("/")}>Galleri Taulo</h1>
             </div>
-                <div id="list">
-                    <Link to="/instagram" id="header-list-links">INSTAGRAM</Link>
-                    <div id="header-list-links" onClick={goToAbout}>MIG</div>
-                    <Link to="/udstillinger" id="header-list-links">UDSTILLINGER</Link>
-                </div>
+            <div id="list">
+                <Link to="/instagram" className="header-list-link-instagram" id="header-list-links" onClick={(e) => removeUnderline(e)}>INSTAGRAM</Link>
+                <div className="header-list-link-about" id="header-list-links" onClick={(e) => {removeUnderline(e); goToAbout()}}>MIG</div>
+                <Link className="header-list-link-exhibition" to="/udstillinger" id="header-list-links" onClick={(e) => removeUnderline(e)}>UDSTILLINGER</Link>
+            </div>
         </div>
      );
 }

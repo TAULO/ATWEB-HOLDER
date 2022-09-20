03ff864bdb61cd490d42f65c7d56ff99ed43707f
Thomas <taulo@live.dk>
2022-09-20 17:27:00 +0200

    Tried to implement page transitions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import "./index.css"
 import LandingPage from './components/LandingPage/LandingPage';
 import InstagramImages from './components/InstagramImages/InstagramImages';
@@ -18,16 +19,28 @@ root.render(
    <Router>
     <Header/>
       <Routes>
-        <Route path="/" element={<LandingPage></LandingPage>}></Route>
-        <Route path='/instagram' element={<InstagramImages/>}></Route>
-        <Route path='/udstillinger' element={<ExhibitionTemplate></ExhibitionTemplate>}></Route>
+        <Route render = {({location}) => {
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames="fade"></CSSTransition>
+          </TransitionGroup>
+        }} path="/" element={<LandingPage></LandingPage>}></Route>
+        <Route render = {({location}) => {
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames="fade"></CSSTransition>
+          </TransitionGroup>
+        }} path='/instagram' element={<InstagramImages/>}></Route>
+        <Route render = {({location}) => {
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames="fade"></CSSTransition>
+          </TransitionGroup>
+        }} path='/udstillinger' element={<ExhibitionTemplate></ExhibitionTemplate>}></Route>
       </Routes>
-    <Footer />
     <Routes>
       <Route path='/admin' element={<AdminLandingPage></AdminLandingPage>}></Route>
       <Route path='/admin/tilfoj-billeder' element={<AddImagesForm></AddImagesForm>}></Route>
       <Route path='/admin/tilfoj-udstillinger' element={<AddExhibtionsForm></AddExhibtionsForm>}></Route>
     </Routes>
+    <Footer />
   </Router>
 );
 
